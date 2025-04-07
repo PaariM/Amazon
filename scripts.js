384f4902d0b9cd6aca8f3ac75aaa7fd471e548ad
PaariM <paarim3@gmail.com>
2025-04-07 10:32:03 +0530

    feat: added all files

---
+++ scripts.js
@@ -0,0 +1,7 @@
+function openModal() {
+    document.getElementById("locationModal").style.display = "flex";
+  }
+
+  function closeModal() {
+    document.getElementById("locationModal").style.display = "none";
+  }
